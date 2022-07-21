d74c6e96728c671ea54e2484948e2905ee0aff38
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-21 14:23:01 -0300

    local storage integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+
+// react task app
+
+// primero crear un input para que el usuario pueda ingresar un nuevo task
+// segundo capturamos el valor del input y lo guardamos en un state
+// tercero guardamos el valor del input en el localStorage
 function App() {
+  const [ newTaskName, setNewTaskName] = useState()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    localStorage.setItem("Tasks", newTaskName)
+    setNewTaskName("")
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a new Task"
+            value={newTaskName}
+            onChange={(e) => setNewTaskName(e.target.value)}
+          />
+          <button>Save task</button>
+      </form>
     </div>
   );
 }
